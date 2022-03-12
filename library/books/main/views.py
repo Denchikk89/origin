0acec5e92b34787e7de8242757ae2f68a0c7a281
Denchikk <lilden09@yandex.ru>
2022-03-12 15:45:42 +0400

    ffuck

--- library/books/main/views.py
+++ library/books/main/views.py
@@ -3,8 +3,8 @@ from.models import Books
 
 
 def index(request):
-    Book = Books.objects.all()
-    return render(request, 'main/index.html', {'Books':Books})
+    book = Books.objects.all()
+    return render(request, 'main/index.html', {'book':book})
 
 
 def about(request):
